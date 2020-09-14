b2af95b5395751a2078478630958d533bb89a45f
Jan Kroeze <jcwkroeze@pm.me>
2020-09-14 17:42:37 +0200

    Import Vulkan lib

---
+++ VulkanSynch.cpp
@@ -0,0 +1,16 @@
+#include "Vulkan.h"
+
+VkSemaphore createSemaphore(VkDevice device) {
+    VkSemaphore result;
+
+    VkSemaphoreCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+    checkSuccess(vkCreateSemaphore(
+        device,
+        &createInfo,
+        nullptr,
+        &result
+    ));
+
+    return result;
+}
