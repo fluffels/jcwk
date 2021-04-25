777befce1ce194652a77fa8eb8f4ba6ec0dab441
Jan CW Kroeze <jcwkroeze@pm.me>
2021-04-25 15:34:17 +0200

    Allow push constants in compute shaders

--- Vulkan/Compute.cpp
+++ Vulkan/Compute.cpp
@@ -3,7 +3,9 @@ void dispatchCompute(
     VulkanPipeline& pipeline,
     uint32_t x,
     uint32_t y,
-    uint32_t z
+    uint32_t z,
+    size_t pushConstantSize = 0,
+    void* pushConstants = nullptr
 ) {
     VkCommandBuffer cmd = allocateCommandBuffer(vk.device, vk.cmdPoolComputeTransient);
     beginOneOffCommandBuffer(cmd);
@@ -19,6 +21,15 @@ void dispatchCompute(
         0, 1, &pipeline.descriptorSet,
         0, nullptr
     );
+    if (pushConstants != nullptr) {
+        vkCmdPushConstants(
+            cmd,
+            pipeline.layout,
+            VK_SHADER_STAGE_COMPUTE_BIT,
+            0, pushConstantSize,
+            pushConstants
+        );
+    }
     vkCmdDispatch(cmd, x, y, z);
     endCommandBuffer(cmd);
     submitCommandBuffer(cmd, vk.computeQueue);
